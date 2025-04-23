9b9fbfcf7271439cfd73efd8ee35e6b5b324d91f
erikscar <erikscarcela@gmail.com>
2025-04-23 15:52:23 -0300

    Add Register Component and function

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+  constructor(private userService: UserService) {}
+
+  registerForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    passwordHash: new FormControl("", [Validators.required]),
+    confirmPassword: new FormControl("", [Validators.required])
+  })
+
+  register(form: FormGroup) {
+    this.userService.register(form.value).subscribe({
+      next: (res) => {
+        console.log("Usuário Registrado com Sucesso: ", res);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  apiUrl: string = "http://localhost:5104/api/User";
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+}
