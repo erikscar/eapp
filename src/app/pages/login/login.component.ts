4cd2ce237a7bd1cda868d33cf1755fb43a4b89b3
erikscar <erikscarcela@gmail.com>
2025-04-28 14:44:34 -0300

    feat: add remember me checbox function to handle session and local storage for JWT

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -14,15 +14,23 @@ export class LoginComponent {
 
   loginForm = new FormGroup({
     email: new FormControl("", [Validators.required, Validators.email]),
-    passwordHash: new FormControl("", [Validators.required])
+    passwordHash: new FormControl("", [Validators.required]),
+    rememberme: new FormControl(false)
   })
 
   login(form: FormGroup) {
     this.userService.login(form.value).subscribe({
       next: (res) => {
-        console.log("Login Realizado com Sucesso: ", res);
+
+        if(!form.get('rememberme')?.value) {
+          localStorage.setItem('token', res.token);
+
+        } else {
+          sessionStorage.setItem('token', res.token);
+        }
+
         this.router.navigate(["/home"]);
-        sessionStorage.setItem('token', res.token);
+        
       },
       error: (err) => {
         console.error(err);
