97a0acd3714aceb93876806b33641e2634ec609c
erikscar <erikscarcela@gmail.com>
2025-05-05 15:57:14 -0300

    feat: added error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
+import { ErrorComponent } from './pages/error/error.component';
+
 
 export const routes: Routes = [
     { path: "", component: LoginComponent, },
     { path: "register", component: RegisterComponent },
     { path: "home", component: HomeComponent },
-    { path: "user", component: UserComponent}
+    { path: "user", component: UserComponent},
+    { path: "**", component: ErrorComponent}
 ];
