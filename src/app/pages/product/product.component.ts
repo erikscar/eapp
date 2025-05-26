c646b47bb2a487338eb7ed4890312c2651867848
erikscar <erikscarcela@gmail.com>
2025-05-26 16:00:58 -0300

    feat: added stars function to handle ratings and add method to post review

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -4,18 +4,27 @@ import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { ReviewService } from '../../services/review.service';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent implements OnInit {
-
-  constructor(private productService: ProductService, private cartService: CartService, private reviewService: ReviewService, private route: ActivatedRoute) { }
   product: any = {};
   reviews: any = [];
+  showReviewBtn = false;
+  stars = [1, 2, 3, 4, 5];
+  reviewForm: FormGroup;
+
+  constructor(private productService: ProductService, private cartService: CartService, private reviewService: ReviewService, private route: ActivatedRoute) {
+    this.reviewForm = new FormGroup({
+      content: new FormControl(""),
+      rating: new FormControl(0)
+    })
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -29,15 +38,7 @@ export class ProductComponent implements OnInit {
         },
       })
 
-      this.reviewService.getAllReviewsFromProduct(Number(params.get('id'))).subscribe({
-        next: (res) => {
-          console.log(res);
-          this.reviews = res;
-        },
-        error: (err) => {
-          console.log(err)
-        },
-      })
+      this.loadReviews(Number(params.get('id')))
     })
   }
 
@@ -52,22 +53,51 @@ export class ProductComponent implements OnInit {
     });
   }
 
-  getStars(rating: number): number[] {
-    return Array(Math.floor(rating))
+ getStars(rating: number): number[] {
+  return Array(Math.round(rating)).fill(0);
+}
+
+  setRating(star: number) {
+    this.reviewForm.patchValue({ rating: star });
   }
 
   getTimeAgo(dateString: string): string {
-  const now = new Date();
-  const date = new Date(dateString);
-  const diffMs = now.getTime() - date.getTime();
-  const diffSec = Math.floor(diffMs / 1000);
-  const diffMin = Math.floor(diffSec / 60);
-  const diffHr = Math.floor(diffMin / 60);
-  const diffDays = Math.floor(diffHr / 24);
+    const now = new Date();
+    const date = new Date(dateString);
+    const diffMs = now.getTime() - date.getTime();
+    const diffSec = Math.floor(diffMs / 1000);
+    const diffMin = Math.floor(diffSec / 60);
+    const diffHr = Math.floor(diffMin / 60);
+    const diffDays = Math.floor(diffHr / 24);
 
-  if (diffSec < 60) return 'há poucos segundos';
-  if (diffMin < 60) return `há ${diffMin} minuto(s)`;
-  if (diffHr < 24) return `há ${diffHr} hora(s)`;
-  return `há ${diffDays} dia(s)`;
-}
+    if (diffSec < 60) return 'há poucos segundos';
+    if (diffMin < 60) return `há ${diffMin} minuto(s)`;
+    if (diffHr < 24) return `há ${diffHr} hora(s)`;
+    return `há ${diffDays} dia(s)`;
+  }
+
+  addReview(form: FormGroup, productId: number) {
+    this.reviewService.addReview(form.value, productId).subscribe({
+      next: (res) => {
+        console.log(res.message)
+        this.loadReviews(productId);
+        this.reviewForm.reset();
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+
+  }
+  loadReviews(productId: number) {
+    this.reviewService.getAllReviewsFromProduct(productId).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.reviews = res;
+      },
+      error: (err) => {
+        console.log(err)
+      },
+    })
+  }
 }
--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -1,15 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
   apiUrl: string = "http://localhost:5104/api/Review"
-  constructor(private htpp: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getAllReviewsFromProduct(productId: number) : Observable<any> {
-    return this.htpp.get(`${this.apiUrl}/${productId}`);
+    return this.http.get(`${this.apiUrl}/${productId}`);
+  }
+
+  addReview(data: any, productId: number): Observable<any> {
+   return this.http.post(`${this.apiUrl}/${productId}`, data);
   }
 }
