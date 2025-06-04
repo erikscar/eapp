9cf15aebd5db2a5f24c6aa05a17ea301425fa39d
erikscar <erikscarcela@gmail.com>
2025-06-04 14:54:27 -0300

    feat: add pagination options in admin user table

--- src/app/pages/admin/users/users.component.ts
+++ src/app/pages/admin/users/users.component.ts
@@ -2,15 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { CommonModule } from '@angular/common';
 import { AdminModalComponent } from '../../../components/admin-modal/admin-modal.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
-  imports: [CommonModule, AdminModalComponent],
+  imports: [CommonModule, FormsModule, AdminModalComponent, NgxPaginationModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
 export class UsersComponent implements OnInit {
   users: any = [];
+  usersPaginated: any = [];
+  currentPage: number = 1;
+  pageSize: number = 5;
   showModal: boolean = false;
 
   constructor(private userService: UserService) {}
@@ -39,4 +44,14 @@ export class UsersComponent implements OnInit {
     this.getUsers();
     this.toggleModal();
   }
+
+  get startIndex(): number {
+    return this.users.length === 0 ? 0 : (this.currentPage -1 ) * this.pageSize + 1
+  }
+
+  get endIndex(): number {
+    const end = this.currentPage * this.pageSize
+    return end > this.users.length ? this.users.length : end;
+  }
+
 }
