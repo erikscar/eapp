d782a7a9be48d0b62da0ab12f4bb1507a8f5e502
erikscar <erikscarcela@gmail.com>
2025-06-06 23:58:48 -0300

    feat: add common admin table component

---
+++ src/app/components/admin-crud-table/admin-crud-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-crud-table',
+  imports: [],
+  templateUrl: './admin-crud-table.component.html',
+  styleUrl: './admin-crud-table.component.scss'
+})
+export class AdminCrudTableComponent {
+
+}
