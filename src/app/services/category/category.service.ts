13893f2b29cb780b6aeacf83dab75cc88c095bca
erikscar <erikscarcela@gmail.com>
2025-06-22 19:51:08 -0300

    feat: add category service

---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Category } from '../../interfaces/Category';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  apiUrl: string = "http://localhost:5104/api/Category"
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+   addCategory(data: Category): Observable<Category> {
+      return this.http.post<Category>(`${this.apiUrl}`, data, { headers: this.authService.getHeaders() })
+    }
+  
+    adminUpdateCategory(data: Category, id: number): Observable<Category> {
+      return this.http.put<Category>(`${this.apiUrl}/${id}`, data, { headers: this.authService.getHeaders() })
+    }
+  
+    removeCategory(id: number): Observable<Category> {
+      return this.http.delete<Category>(`${this.apiUrl}/${id}`, { headers: this.authService.getHeaders() });
+    }
+}
